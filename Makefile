7e33c943f9eeefc2a8a3862535c7a09085b1842a
godfather239 <greenday239@sina.com>
2015-10-02 18:13:31 +0800

    add sort and partion and random_select codes

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
 CXX = g++
 CXXFLAGS = -g
 
-OUTPUT = binary_tree
+OUTPUT = binary_tree sort
 all: $(OUTPUT)
 
 SRC := $(wildcard *.cpp)
-OBJS := $(SRC:.CPP=.O)
+OBJS := $(SRC:.CPP=.o)
 
 binary_tree : binary_tree.o
 	$(CXX) -o $@ $< $(CXXFLAGS)
+sort : sort.o
+	$(CXX) -o $@ $< $(CXXFLAGS)
+
 #sort : sort.cpp
 #	$(CXX) -o $@ $< $(CXXFLAGS)
 #linklist : linklist.cpp
--- sort.cpp
+++ sort.cpp
@@ -7,15 +7,45 @@
 typedef int (*sort_func_ptr) (int*, int);
 
 int partition(int* a, int p, int r);
+int random_partition(int* a, int p, int r);
 int test_partition();
 int print_array(int* a, int n);
 int quicksort(int* a, int n);
 int test_sort(int n, sort_func_ptr sf);
 
+/**
+ * @brief       Select the k-th number in array by ascending order. Notice that the function
+ *              will change order of array elements.
+ * @param[in]   array pointer
+ * @param[in]   array length
+ * @param[in]   the k-th number index, k is in range:[1,n]
+ * @return
+ *              index of the k-th number, -1 if error happens
+ **/
+int select_asc(int* a, int n, int k);
+int print_kmin(int* a, int n, int k);
+
 
 // The simpliest partition implementaion which chooses arr[r] as pivot by default
 int partition(int* a, int p, int r)
 {
+    while (p < r) {
+        while ((p < r) && (a[p] <= a[r]))
+            ++p;
+        if (p < r)
+            std::swap(a[p], a[r--]);
+        while ((p < r) && (a[p] <= a[r]))
+            --r;
+        if (p < r)
+            std::swap(a[p++], a[r]);
+    }
+    return p;
+}
+
+// Enhance stability of partition.
+int random_partition(int* a, int p, int r)
+{
+    std::swap(a[(rand()%(r-p+1))+p], a[r]);
     while (p < r) {
         while ((p < r) && (a[p] <= a[r]))
             ++p;
@@ -56,7 +86,8 @@ int quicksort(int* a, int n)
 {
     if (!a || n <= 0)
         return -1;
-    int pivot = partition(a, 0, n - 1);
+    //int pivot = partition(a, 0, n - 1);
+    int pivot = random_partition(a, 0, n - 1);
     quicksort(a, pivot);
     quicksort(a + pivot + 1, n - pivot - 1);
     return 0;
@@ -78,6 +109,44 @@ int test_sort(int n, sort_func_ptr sf)
     printf("After sort: \n");
     print_array(a, n);
 
+    delete[] a;
+    return 0;
+}
+
+int select_asc(int* a, int n, int k)
+{
+    if (!a || n <= 0 || k <= 0 || k > n)
+        return -1;
+    int p = random_partition(a, 0, n - 1);
+    if (p == k - 1)
+        return p;
+    else if (p > k - 1)
+        return select_asc(a, p, k);
+    else
+        return p + 1 + select_asc(a + p + 1, n - p - 1, k - p - 1);
+}
+
+int test_select()
+{
+    int a[10] = {1, 15, 7, 6, 5, 10, 8, 3, 4, 9};
+    for (int i = 1; i <= 7; ++i) {
+        printf("Test %d:\n", i);
+        int index = select_asc(a, 10, i);
+        if (index >= 0 && index < 10)
+            printf("Found the %d-th number in array, result index: %d, number: %d\n", i, index, a[index]);
+        else
+            printf("Error on finding the %d-th number in array, result index: %d\n", i, index);
+    }
+    return 0;
+}
+
+int print_kmin(int* a, int n, int k)
+{
+    if (!a || n <= 0 || k <= 0 || k > n)
+        return -1;
+    int p = select_asc(a, n, k);
+    printf("The min-%d elements in array are: \n", k);
+    print_array(a, p+1);
     return 0;
 }
 
@@ -89,7 +158,13 @@ int main(int argc, char* argv[])
     }
 
     srand(time(NULL));
-    test_sort(atoi(argv[1]), quicksort);
+    //for (int i = 0; i < 100; ++i) {
+    //    printf("Test %d: \n", i+1);
+    //    test_sort(atoi(argv[1]), quicksort);
+    //}
+    test_select();
+    int a[10] = {1, 15, 7, 6, 5, 10, 8, 3, 4, 9};
+    print_kmin(a, 10, 5);
 
     //test_partition();
     return 0;
