57edda4fd235d29fe8fbf52d66f2be56f7f78623
godfather239 <greenday239@sina.com>
2015-10-04 11:52:05 +0800

    modify search.cpp

--- search.cpp
+++ search.cpp
@@ -31,23 +31,88 @@ int find_missing_num(int* arr, int size, int mask, int& out)
     return 0;
 }
 
+// Find missing number iteratively
+int find_missing_iter(int* arr, int size, int mask, int& out)
+{
+    if (!arr || size <= 0 || mask <= 0)
+        return -1;
+    int* left = arr;
+    int* right = arr + size - 1;
+    while ((left <= right) && (mask > 0)) {
+        int* curr = left;
+        int* leftend = left;
+        while (curr <= right) {
+            // The numbers which have 0 on mask bit will be saved at left.
+            if ((*curr & mask) == 0) 
+                std::swap(*curr, *leftend++);
+            ++curr;
+        }
+        // Chose smaller segment
+        if ((leftend - left) <= (right - leftend + 1)) {
+            out &= ~mask;  // Set bit to 0
+            right = leftend - 1;
+        } else {
+            out |= mask;  // Set bit to 1
+            left = leftend;
+        }
+        mask >>= 1;
+    }
+    return 0;
+}
+
+// limit is 2^k
+int find_missing(int* arr, int size, int limit, int& out)
+{
+    if (!arr || size <= 0 || limit <= 0)
+        return -1;
+    int mask = limit >> 1;
+    int* left = arr;
+    int* right = arr + size - 1;
+    while ((left <= right) && (mask > 0)) {
+        int* curr = left;
+        int* leftend = left;
+        while (curr <= right) {
+            // The numbers which have 0 on mask bit will be saved at left.
+            if ((*curr & mask) == 0) 
+                std::swap(*curr, *leftend++);
+            ++curr;
+        }
+        // Chose smaller segment
+        if ((leftend - left) <= (right - leftend + 1)) {
+            out &= ~mask;  // Set bit to 0
+            right = leftend - 1;
+        } else {
+            out |= mask;  // Set bit to 1
+            left = leftend;
+        }
+        mask >>= 1;
+    }
+    return 0;
+}
+
 int test_find()
 {
     srand(time(NULL));
+    /**
+     * NOTICE:  MAX should be 2^k, k is in range [1, 32]; mask is 2^(k-1)
+     **/
     const int MAX = 8;
-    UnrepeatedRand rand(MAX);
-    int arr[7] = {0};
+    const int len = 7;
     int mask = 4;
+    UnrepeatedRand rand(MAX);
+    int arr[len] = {0};
 
     for (int testtime = 1; testtime <= 10; ++testtime) {
         rand.reset();
-        for (int i = 0; i < 7; ++i) {
+        for (int i = 0; i < len; ++i) {
             arr[i] = rand.next();
         }
         printf("Test time: %d, array is: \n", testtime);
-        print_array(arr, 7);
+        print_array(arr, len);
         int out = 0;
-        find_missing_num(arr, 7, mask, out);
+        //find_missing_num(arr, len, mask, out);
+        //find_missing_iter(arr, len, mask, out);
+        find_missing(arr, len, MAX, out);
         printf("Missing num: %d\n", out);
     }
     return 0;
